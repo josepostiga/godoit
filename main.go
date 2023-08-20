308e379e7cb9ec3f2b858c84c6c2001c770dd08b
José Postiga <josepostiga@icloud.com>
2023-08-20 08:41:09 +0100

    refactor: Allow changing HTTP port to use via env vars

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/joho/godotenv"
 	health_check "godoit/health-check"
 	"godoit/tasks"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -19,5 +21,12 @@ func main() {
 	health_check.RegisterRoutes(r)
 	tasks.RegisterRoutes(r)
 
-	log.Fatalf("Couldn't start server: %s", http.ListenAndServe(":3000", r))
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		fmt.Println("HTTP_PORT not set, using default port")
+		port = "8080"
+	}
+	fmt.Printf("Starting server on port %s", port)
+
+	log.Fatalf("Couldn't start server: %s", http.ListenAndServe(":"+port, r))
 }
