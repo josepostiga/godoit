07131ec7257aa00ea995b99cc3f01e67875b0a89
José Postiga <josepostiga@icloud.com>
2023-08-21 07:15:00 +0100

    build: Add DB migrations

---
+++ migrations/20230820080719_create_tasks_table.up.sql
@@ -0,0 +1,7 @@
+create table if not exists tasks (
+    id int primary key generated always as identity,
+    title text not null,
+    description text,
+    created_at timestamptz null default current_timestamp,
+    updated_at timestamptz null default current_timestamp
+);
